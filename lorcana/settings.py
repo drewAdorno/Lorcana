454d32490506a5976a26f551a28537c7b1366d67
Drew <drew.adorno@gmail.com>
2024-10-04 01:24:46 -0400

    adding static files

--- lorcana/settings.py
+++ lorcana/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for lorcana project.
 
@@ -117,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where static files will be collected
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
